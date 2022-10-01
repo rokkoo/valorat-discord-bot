42f21fc9cbc298f1255621686252642c4aee4764
Juan Castellano <juancastellano115@gmail.com>
2022-10-01 11:56:17 +0200

    Feat: Rank command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,5 @@
 import { Message } from 'discord.js';
-
+import axios from 'axios';
 interface Command {
   command: string;
   action: (message: Message, args: string[]) => void;
@@ -14,7 +14,41 @@ const helloCommand: Command = {
   },
 };
 
-const commands: Command[] = [helloCommand];
+const RankCommand: Command = {
+  command: 'rank',
+  async action(message, args) {
+  
+    if (args.length === 0) {
+     return message.reply(`Provide a username and tag! (Fulanin#EUW)`);
+    }
+    // Handling names with spaces
+    const rawArgs = args.join(' ');
+    const splittedArgs = rawArgs.split('#');
+    // Encode in URL format
+    const name = encodeURI(splittedArgs[0]);
+    const tag = splittedArgs[1];
+
+    if (!name || !tag) {
+      return message.reply(`Provide a username and tag! (Fulanin#EUW)`);
+     }
+
+    const response = await axios.get(`https://api.henrikdev.xyz/valorant/v1/mmr/eu/${name}/${tag}`);
+    if (response.status === 200) {
+      const { data: { data: {currenttierpatched, elo } } } = response;
+      
+      if (!currenttierpatched && !elo) {
+        return message.reply(`${decodeURI(name)}#${tag} doesn't have MMR stats at this moment.`);
+      }
+      message.reply(`${decodeURI(name)}#${tag}'s current tier is ${currenttierpatched}, ELO: ${elo}.`);
+    } else {
+      message.reply(`Oops something went wrong!`);
+    }
+ 
+  },
+};
+
+
+const commands: Command[] = [helloCommand, RankCommand];
 
 export const handleCommand = (message: Message) => {
   const MESSAGE_ID = message.author.id;
