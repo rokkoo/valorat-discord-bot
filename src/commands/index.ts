b66dc7547793b99dbb3e3c9707e7f07b0a6d68ff
Juan Castellano <juancastellano115@gmail.com>
2022-10-07 20:02:56 +0200

    Chore: Refactor rank command and create constants

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,5 @@
 import { Message } from 'discord.js';
-import axios from 'axios';
+import { getPlayerNameAndTag, getPlayerRank } from './lib/rank'
 interface Command {
   command: string;
   action: (message: Message, args: string[]) => void;
@@ -21,29 +21,9 @@ const RankCommand: Command = {
     if (args.length === 0) {
      return message.reply(`Provide a username and tag! (Fulanin#EUW)`);
     }
-    // Handling names with spaces
-    const rawArgs = args.join(' ');
-    const splittedArgs = rawArgs.split('#');
-    // Encode in URL format
-    const name = encodeURI(splittedArgs[0]);
-    const tag = splittedArgs[1];
-
-    if (!name || !tag) {
-      return message.reply(`Provide a username and tag! (Fulanin#EUW)`);
-     }
-
-    const response = await axios.get(`https://api.henrikdev.xyz/valorant/v1/mmr/eu/${name}/${tag}`);
-    if (response.status === 200) {
-      const { data: { data: {currenttierpatched, elo } } } = response;
-      
-      if (!currenttierpatched && !elo) {
-        return message.reply(`${decodeURI(name)}#${tag} doesn't have MMR stats at this moment.`);
-      }
-      message.reply(`${decodeURI(name)}#${tag}'s current tier is ${currenttierpatched}, ELO: ${elo}.`);
-    } else {
-      message.reply(`Oops something went wrong!`);
-    }
+   const { name, tag } = getPlayerNameAndTag(args);
  
+   return message.reply(await getPlayerRank(name, tag));
   },
 };
 
@@ -54,7 +34,7 @@ export const handleCommand = (message: Message) => {
   const MESSAGE_ID = message.author.id;
 
   // dont continue as is the bot `test-valorant-bot`
-  if (MESSAGE_ID === '1016773153411833856') return;
+  if (MESSAGE_ID === '1025682821941039155') return;
 
   const [fullCommand, ...args] = message.content.split(' ');
   const [_, command] = fullCommand.split('!'); // get the command
---
+++ src/commands/lib/constants.ts
@@ -0,0 +1,4 @@
+export class Settings {
+  static readonly API_VERSION = "v1";
+  static readonly BASE_URL = `https://api.henrikdev.xyz/valorant/${this.API_VERSION}`;
+}
